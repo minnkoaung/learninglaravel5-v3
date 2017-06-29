bcc826319a108605438765b78a2ae1faf722fcf3
minnkoaung <minnkoaung@gmail.com>
2017-06-29 17:02:39 +0630

    material design added

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,13 +1,30 @@
 <head>
     <title> @yield('title') </title>
+     <!-- Material Design fonts -->
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/icon?family=Material+Icons">
+
     <link rel="stylesheet" href="/css/app.css">
-    <script src="/js/app.js"></script>
+     <!-- Bootstrap Material Design -->
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap-material-design.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/ripples.min.css">
+
 </head>
 <body>
 
 @include('navbar')
 
 @yield('content')
+
+<script src="/js/app.js"></script>
+<script src="/js/ripples.min.js"></script>
+<script src="/js/material.min.js"></script>
+<script>
+    $(document).ready(function() {
+        // This command is used to initialize some elements and make them work properly
+        $.material.init();
+    });
+</script>
 
 </body>
 </html>
